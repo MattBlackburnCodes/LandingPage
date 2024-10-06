2be8454966c338f04007536754e39c7d3d2267c2
SoliBlack <SolitaireBlack21@gmail.com>
2024-10-06 00:36:18 -0400

    changed the button layout in modals

--- src/Components/PortfolioModal.jsx
+++ src/Components/PortfolioModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function PortfolioModal({ project, handleCloseModal, props }) {
+export default function PortfolioModal({ project, handleCloseModal }) {
     return (
         <div className="modal fade show" style={{ display: 'block' }} tabIndex="-1">
             <div className="modal-dialog modal-xl">
@@ -15,13 +15,7 @@ export default function PortfolioModal({ project, handleCloseModal, props }) {
                                     <h2 className="portfolio-modal-title text-secondary text-uppercase mb-0">
                                         {project.name}
                                     </h2>
-                                    <div className="divider-custom">
-                                        <div className="divider-custom-line"></div>
-                                        <div className="divider-custom-icon">
-                                            <img src={props.image} alt="Logo" className="logoPic" />
-                                        </div>
-                                        <div className="divider-custom-line"></div>
-                                    </div>
+                                    
                                     <img className="img-fluid rounded mb-5" src={project.image} alt={project.name} />
                                     <h5>Description</h5>
                                     <p className="mb-4">{project.description}</p>
